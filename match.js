8823e95f7b28fe95c56fd8c87252fb53d0be9a64
Sam Abdelfattah <samfatth@gmail.com>
2017-08-31 14:04:07 -0600

    Match game

---
+++ match.js
@@ -0,0 +1,189 @@
+$(document).ready(function(){
+
+  var arr = [
+    1, 2, 3, 4,
+    5, 6, 7, 8,
+    1, 2, 3, 4,
+    5, 6, 7, 8
+  ];
+
+$('.choice')on('click' function() {
+  $("#" + this.id).removeClass('hiddden');
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    //var yep = arr[Math.floor(Math.random(arr))];
+// var yep = arr[Math.floor(Math.random())]
+//   $('.choice').on('click', function(){
+//       // pull from array; 
+//     console.log(yep);
+//     // var yep = [Math.floor((Math.random()* 8) + 1)];
+//       var ham = ('yep');
+//     })
+//     $('#col2').on('click', function(){
+//       // pull from array)
+//       console.log(yep);
+//       // var yep = [Math.floor((Math.random()* 8) + 1)];
+//       // alert
+//       // displayText(yep);    
+//     })
+//     $('#col3').on('click', function(){
+//       // pull from array;
+//       console.log(yep);
+//       // var yep = [Math.floor((Math.random()* 8) + 1)];
+//       // alert
+//       // displayText(yep);
+//     })
+//     $('#col4').on('click', function(){
+//       // pull from array;
+//       console.log(yep);
+//       // var yep = [Math.floor((Math.random()* 8) + 1)];
+//       // alert
+//       // displayText(yep); 
+//     })
+//     $('#col5').on('click', function(){
+//       // pull from array;
+//       console.log(yep);
+//       // var yep = [Math.floor((Math.random()* 8) + 1)];
+//       // alert
+//       // displayText(yep); 
+//     })
+//     $('#col6').on('click', function(){
+//       // pull from array;
+//       console.log(yep);
+//       // var yep = [Math.floor((Math.random()* 8) + 1)];
+//       // alert
+//       // displayText(yep); 
+//     })
+//     $('#col7').on('click', function(){
+//       // pull from array;
+//       console.log(yep);
+//       // var yep = [Math.floor((Math.random()* 8) + 1)];
+//       // alert
+//       // displayText(yep); 
+//     })
+//     $('#col8').on('click', function(){
+//       // pull from array;
+//       console.log(yep);
+//       // var yep = [Math.floor((Math.random()* 8) + 1)];
+//       // alert
+//       // displayText(yep); 
+//     })
+//     $('#col9').on('click', function(){
+//       // pull from array;
+//       console.log(yep);
+//       // var yep = [Math.floor((Math.random()* 8) + 1)];
+//       // alert
+//       // displayText(yep);   
+//     })
+//     $('#col10').on('click', function(){
+//       // pull from array;
+//       console.log(yep);
+//       // var yep = [Math.floor((Math.random()* 8) + 1)];
+//       // alert
+//       // displayText(yep); 
+//       })
+//     $('#col11').on('click', function(){
+//       // pull from array;
+//       console.log(yep);
+//       // var yep = [Math.floor((Math.random()* 8) + 1)];
+//       // alert
+//       // displayText(yep); 
+//       })
+//     $('#col12').on('click', function(){
+//       // pull from array;
+//       console.log(yep);
+//       // var yep = [Math.floor((Math.random()* 8) + 1)];
+//       // alert
+//       // displayText(yep); 
+//       })
+//     $('#col13').on('click', function(){
+//       // pull from array;
+//       console.log(yep);
+//       // var yep = [Math.floor((Math.random()* 8) + 1)];
+//       // alert
+//       // displayText(yep); 
+//       })
+//     $('#col14').on('click', function(){
+//       // pull from array;
+//       console.log(yep);
+//       // var yep = [Math.floor((Math.random()* 8) + 1)];
+//       // alert
+//       // displayText(yep); 
+//       })
+//     $('#col15').on('click', function(){
+//       // pull from array;
+//       console.log(yep);
+//       // var yep = [Math.floor((Math.random()* 8) + 1)];
+//       // alert
+//       // displayText(yep);
+//       })
+//     $('#col16').on('click', function(){
+//       // pull from array;
+//       console.log(yep);
+//       // var yep = [Math.floor((Math.random()* 8) + 1)];
+//       // alert
+//       // displayText(yep);
+//     })
+   
+
+
+  
+  // var pick1 = $('#picks'); 
+  // var pick2 = $('#picks');
+  // if pick1 = pick2
+  //   console.log("It's a match!");
+  // else if pick1 != pick2
+  //   console.log("Sorry try again!")
+
+// I want to put each number int a 'cart' 
+// and use an if statement to match them
+// limit the user to two choices
+// function displayText() {
+//     var inCart = $('#in-cart');
+//     // var cart = ('#cart');
+//     var choice = $('.choice:first');
+//     console.log(choice.text())
+//     // var installment =  
+//     // inCart.append('<li>' + );
+//     inCart.displayText(ham);
+//   }
+
+  
+  
+  
+})
